528d40d90d325a6060616de199dfbe2afef321b0
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-03-27 15:03:22 +0900

    タスク管理ページ追加

--- src/app/manegement/page.tsx
+++ src/app/manegement/page.tsx
@@ -1,9 +1,13 @@
+"use client"
+
+import  DataGridDemo  from "@/components/DataGridDemo"
 import styles from "./page.module.css"
 
 export default function Manegement(){
     return(
         <div>
-            <h1 className={styles.text}>これは就活・インターンのタスク管理サイトです</h1>
+            <h1 className={styles.text}>これは就活・インターンのタスク管理ページです</h1>
+            <DataGridDemo />
         </div>
     )
 }
